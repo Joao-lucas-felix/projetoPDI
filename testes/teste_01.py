d062ccf04192b18c3532694296cc49aa21f4c7fb
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-03-17 15:56:54 -0300

    Project Initialzr
    Project initializing and, implementation of the conversion rgb to hsv methods

---
+++ testes/teste_01.py
@@ -0,0 +1,8 @@
+from PIL import Image
+from Conversor import  image_to_hsv_format
+from Conversor import hsv_matriz_to_image
+
+image = Image.open("./testes/Images/img2.jpg")
+hsv_image = image_to_hsv_format(image)
+image2 = hsv_matriz_to_image(hsv_image)
+image2.show()
